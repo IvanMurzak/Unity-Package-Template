6db592bef9d13bdc08711b0c81e14d59f52a8dca
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2025-04-03 00:08:34 -0700

    Update clean_template.bat

--- Commands/clean_template.bat
+++ Commands/clean_template.bat
@@ -6,7 +6,7 @@ for /r %%f in (*.meta) do (
     del /f /q "%%f"
 )
 
-del /f /q "Packages/packages-lock.json"
+del /f /q "Packages\packages-lock.json"
 del /f /q "Unity-Package-Template.sln"
 
 echo Done!
