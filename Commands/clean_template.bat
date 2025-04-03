48b47f505c8a2fc5b7327d5de7643aa242715c50
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2025-04-03 00:07:41 -0700

    Update clean_template.bat

--- Commands/clean_template.bat
+++ Commands/clean_template.bat
@@ -1,12 +1,12 @@
 cd ..
 rmdir /s /q "ProjectSettings"
-rmdir /s /q "Packages"
 rmdir /s /q ".vscode"
 
 for /r %%f in (*.meta) do (
     del /f /q "%%f"
 )
 
+del /f /q "Packages/packages-lock.json"
 del /f /q "Unity-Package-Template.sln"
 
 echo Done!
